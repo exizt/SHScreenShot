be3ceb27ffaf7abdb6d5f02e5744cc01ac60625a
e2xist <e2xist@gmail.com>
2017-03-10 02:15:14 +0900

    TestImage -> SampleImage 로 교체. 이 프로젝트 는 image compress option 을 확인하기 위한 목적

--- SampleImage/Form1.Designer.cs
+++ SampleImage/Form1.Designer.cs
@@ -31,7 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox_Before = new System.Windows.Forms.PictureBox();
-            this.pictureBox_Result = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Result1 = new System.Windows.Forms.PictureBox();
             this.pictureBox_Result2 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -49,8 +49,11 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_title = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Before)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result4)).BeginInit();
@@ -63,9 +66,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(186, 23);
+            this.button1.Location = new System.Drawing.Point(13, 254);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(167, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = ">>";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,17 +83,17 @@
             this.pictureBox_Before.TabIndex = 1;
             this.pictureBox_Before.TabStop = false;
             // 
-            // pictureBox_Result
+            // pictureBox_Result1
             // 
-            this.pictureBox_Result.Location = new System.Drawing.Point(288, 23);
-            this.pictureBox_Result.Name = "pictureBox_Result";
-            this.pictureBox_Result.Size = new System.Drawing.Size(164, 153);
-            this.pictureBox_Result.TabIndex = 2;
-            this.pictureBox_Result.TabStop = false;
+            this.pictureBox_Result1.Location = new System.Drawing.Point(200, 70);
+            this.pictureBox_Result1.Name = "pictureBox_Result1";
+            this.pictureBox_Result1.Size = new System.Drawing.Size(164, 153);
+            this.pictureBox_Result1.TabIndex = 2;
+            this.pictureBox_Result1.TabStop = false;
             // 
             // pictureBox_Result2
             // 
-            this.pictureBox_Result2.Location = new System.Drawing.Point(463, 23);
+            this.pictureBox_Result2.Location = new System.Drawing.Point(375, 70);
             this.pictureBox_Result2.Name = "pictureBox_Result2";
             this.pictureBox_Result2.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result2.TabIndex = 3;
@@ -99,7 +102,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(288, 180);
+            this.label1.Location = new System.Drawing.Point(200, 55);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 12);
             this.label1.TabIndex = 4;
@@ -108,7 +111,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(461, 179);
+            this.label2.Location = new System.Drawing.Point(373, 54);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 12);
             this.label2.TabIndex = 4;
@@ -116,7 +119,7 @@
             // 
             // pictureBox_Result3
             // 
-            this.pictureBox_Result3.Location = new System.Drawing.Point(648, 23);
+            this.pictureBox_Result3.Location = new System.Drawing.Point(560, 70);
             this.pictureBox_Result3.Name = "pictureBox_Result3";
             this.pictureBox_Result3.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result3.TabIndex = 3;
@@ -125,7 +128,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(646, 179);
+            this.label3.Location = new System.Drawing.Point(558, 54);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 12);
             this.label3.TabIndex = 4;
@@ -133,7 +136,7 @@
             // 
             // pictureBox_Result4
             // 
-            this.pictureBox_Result4.Location = new System.Drawing.Point(288, 194);
+            this.pictureBox_Result4.Location = new System.Drawing.Point(200, 275);
             this.pictureBox_Result4.Name = "pictureBox_Result4";
             this.pictureBox_Result4.Size = new System.Drawing.Size(164, 153);
             this.pictureBox_Result4.TabIndex = 2;
@@ -141,7 +144,7 @@
             // 
             // pictureBox_Result5
             // 
-            this.pictureBox_Result5.Location = new System.Drawing.Point(463, 194);
+            this.pictureBox_Result5.Location = new System.Drawing.Point(375, 275);
             this.pictureBox_Result5.Name = "pictureBox_Result5";
             this.pictureBox_Result5.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result5.TabIndex = 3;
@@ -149,7 +152,7 @@
             // 
             // pictureBox_Result6
             // 
-            this.pictureBox_Result6.Location = new System.Drawing.Point(648, 194);
+            this.pictureBox_Result6.Location = new System.Drawing.Point(560, 275);
             this.pictureBox_Result6.Name = "pictureBox_Result6";
             this.pictureBox_Result6.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result6.TabIndex = 3;
@@ -158,7 +161,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(288, 351);
+            this.label4.Location = new System.Drawing.Point(200, 257);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(43, 12);
             this.label4.TabIndex = 4;
@@ -167,7 +170,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(461, 350);
+            this.label5.Location = new System.Drawing.Point(373, 256);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(43, 12);
             this.label5.TabIndex = 4;
@@ -176,7 +179,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(646, 350);
+            this.label6.Location = new System.Drawing.Point(558, 256);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(43, 12);
             this.label6.TabIndex = 4;
@@ -184,7 +187,7 @@
             // 
             // pictureBox_Result7
             // 
-            this.pictureBox_Result7.Location = new System.Drawing.Point(288, 365);
+            this.pictureBox_Result7.Location = new System.Drawing.Point(200, 480);
             this.pictureBox_Result7.Name = "pictureBox_Result7";
             this.pictureBox_Result7.Size = new System.Drawing.Size(164, 153);
             this.pictureBox_Result7.TabIndex = 2;
@@ -192,7 +195,7 @@
             // 
             // pictureBox_Result8
             // 
-            this.pictureBox_Result8.Location = new System.Drawing.Point(463, 365);
+            this.pictureBox_Result8.Location = new System.Drawing.Point(375, 480);
             this.pictureBox_Result8.Name = "pictureBox_Result8";
             this.pictureBox_Result8.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result8.TabIndex = 3;
@@ -200,7 +203,7 @@
             // 
             // pictureBox_Result9
             // 
-            this.pictureBox_Result9.Location = new System.Drawing.Point(648, 365);
+            this.pictureBox_Result9.Location = new System.Drawing.Point(560, 480);
             this.pictureBox_Result9.Name = "pictureBox_Result9";
             this.pictureBox_Result9.Size = new System.Drawing.Size(170, 153);
             this.pictureBox_Result9.TabIndex = 3;
@@ -209,7 +212,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(288, 523);
+            this.label7.Location = new System.Drawing.Point(200, 463);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(43, 12);
             this.label7.TabIndex = 4;
@@ -218,7 +221,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(461, 521);
+            this.label8.Location = new System.Drawing.Point(373, 461);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(43, 12);
             this.label8.TabIndex = 4;
@@ -227,17 +230,50 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(646, 521);
+            this.label9.Location = new System.Drawing.Point(558, 461);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(43, 12);
             this.label9.TabIndex = 4;
             this.label9.Text = "TEST9";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "interpolation 확대";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label_title.Location = new System.Drawing.Point(196, 23);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(72, 19);
+            this.label_title.TabIndex = 6;
+            this.label_title.Text = "label10";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "interpolation 축소";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(848, 557);
+            this.ClientSize = new System.Drawing.Size(747, 653);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label_title);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
@@ -255,13 +291,13 @@
             this.Controls.Add(this.pictureBox_Result5);
             this.Controls.Add(this.pictureBox_Result4);
             this.Controls.Add(this.pictureBox_Result2);
-            this.Controls.Add(this.pictureBox_Result);
+            this.Controls.Add(this.pictureBox_Result1);
             this.Controls.Add(this.pictureBox_Before);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Before)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Result4)).EndInit();
@@ -278,7 +314,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox_Result;
+        private System.Windows.Forms.PictureBox pictureBox_Result1;
         public System.Windows.Forms.PictureBox pictureBox_Before;
         private System.Windows.Forms.PictureBox pictureBox_Result2;
         private System.Windows.Forms.Label label1;
@@ -297,6 +333,9 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_title;
+        private System.Windows.Forms.Button button3;
     }
 }
 
