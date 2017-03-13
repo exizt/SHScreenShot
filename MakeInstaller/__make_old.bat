2a48ccf8e0b7e60abd0ec9d42bac2e9c37dedfab
e2xist <e2xist@gmail.com>
2017-03-13 23:25:42 +0900

    설치 인스톨러 수정

--- MakeInstaller/__make_old.bat
+++ MakeInstaller/__make_old.bat
@@ -1,5 +1,4 @@
-@ECHO OFF
 copy ..\application\bin\Release\SHScreenCapture.exe .
-if not exist installer (mkdir installer)
+mkdir installer
 "C:\Program Files (x86)\NSIS\makensis.exe" _make.nsi
 pause
