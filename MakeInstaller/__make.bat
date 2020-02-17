32e32ad0f699396ce6f8a155ebd1adbf135aa50f
e2xist <e2xist@gmail.com>
2020-02-17 12:13:17 +0900

    FolderBrowserDialog  -> CommonOpenFileDialog

--- MakeInstaller/__make.bat
+++ MakeInstaller/__make.bat
@@ -1,4 +1,6 @@
-copy ..\application\bin\Release\SHScreenCapture.exe .
+copy ..\application\bin\Release\SHScreenCapture.exe .\source
+copy ..\application\bin\Release\Microsoft.WindowsAPICodePack.dll .\source
+copy ..\application\bin\Release\Microsoft.WindowsAPICodePack.Shell.dll .\source
 @echo off
 if not exist installer (mkdir installer)
 "C:\Program Files (x86)\NSIS\makensis.exe" /INPUTCHARSET UTF8 _make.nsi
