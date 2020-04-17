94d8a57a00f2442b689c9cb7e22a5a4774680527
e2xist <e2xist@gmail.com>
2020-04-17 18:27:54 +0900

    minor update

--- MakeInstaller/__make.bat
+++ MakeInstaller/__make.bat
@@ -1,7 +1,8 @@
+if not exist source (mkdir source)
+if not exist installer (mkdir installer)
 copy ..\application\bin\Release\SHScreenCapture.exe .\source
 copy ..\application\bin\Release\Microsoft.WindowsAPICodePack.dll .\source
 copy ..\application\bin\Release\Microsoft.WindowsAPICodePack.Shell.dll .\source
 @echo off
-if not exist installer (mkdir installer)
 "C:\Program Files (x86)\NSIS\makensis.exe" /INPUTCHARSET UTF8 _make.nsi
 pause
